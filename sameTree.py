465dbf55dd0faecb83759238237de2ba768cd575
Joshua Catelino <joshuacastelino2002@hotmail.com>
2022-05-04 00:58:55 +0100

    sameTree

---
+++ sameTree.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p or not q:
+            return p == q
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
